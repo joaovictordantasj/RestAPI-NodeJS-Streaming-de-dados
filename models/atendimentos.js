3272ba0c32f32d9b9c0ac496bc6852277327b77f
João Victor <jv@pop-os.localdomain>
2021-02-10 09:25:32 -0300

    Parte de serviços criada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const axios = require('axios');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
@@ -56,11 +57,14 @@ class Atendimento {
   buscaPorId (id, res) {
     const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
 
-    conexao.query(sql, (err, resultados) => {
+    conexao.query(sql, async (err, resultados) => {
       const atendimento = resultados[0];
+      const cpf = atendimento.cliente;
       if (err) {
         res.status(400).json(err);
       } else {
+        const { data } = await axios.get(`http://localhost:8082/${cpf}`);
+        atendimento.cliente = data;
         res.status(200).json(atendimento);
       }
     })
